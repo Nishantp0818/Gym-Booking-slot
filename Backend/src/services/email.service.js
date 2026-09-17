c8b1f01b088ee3117784c1f75d0feb7938f21642
Nishant Pandey <nishantpandey806@.gmail.com>
2026-09-17 13:22:11 +0530

    Fix SMTP IPv4 connection on Render

--- Backend/src/services/email.service.js
+++ Backend/src/services/email.service.js
@@ -5,7 +5,9 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
-    secure: false,
+    secure: false,  
+      family: 4,
+
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
